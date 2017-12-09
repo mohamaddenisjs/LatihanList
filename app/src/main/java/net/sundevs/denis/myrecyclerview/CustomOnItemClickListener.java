a4479ace8544dcc81f952a16f9c2d8dfb87ffc7a
mohamaddenisjs <mohamaddenisjs@gmail.com>
2017-12-09 10:11:50 +0700

    Initial commit

---
+++ app/src/main/java/net/sundevs/denis/myrecyclerview/CustomOnItemClickListener.java
@@ -0,0 +1,26 @@
+package net.sundevs.denis.myrecyclerview;
+
+import android.view.View;
+
+/**
+ * Created by Denis on 15/11/2017.
+ */
+
+public class CustomOnItemClickListener implements View.OnClickListener{
+
+    private int position;
+    private OnItemClickCallback onItemClickCallback;
+    public CustomOnItemClickListener(int position, OnItemClickCallback onItemClickCallback){
+        this.position = position;
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemClickCallback.onItemClicked(view, position);
+    }
+
+    public interface OnItemClickCallback{
+        void onItemClicked(View view, int position);
+    }
+}
